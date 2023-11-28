d4e936878873e4c9f2a09cc7d70fbee87721c78e
Switchy131933 <zukagugua@gmail.com>
2023-11-28 12:59:10 +0400

    name of commit

--- java.js
+++ java.js
@@ -1,27 +1,30 @@
 let kamateli = document.querySelector('.kamateli')
 let noli = document.querySelector('.noli')
-let asd = 1
 let h1 = document.querySelector('.es')
-let gif = document.querySelector('.gif')
+let loader = document.querySelector('.loader')
+
+
+h1.innerHTML = getData();
+noli.innerHTML = getData()
 
 
 kamateli.addEventListener('click', () => {
-    noli.innerHTML = asd++
     getData();
 })
 
 
+
 async function getData(){
-    h1.innerHTML = "loading..."
-
-
+    h1.innerHTML = ""
+    loader.classList.add("my");
+    noli.innerHTML = "wait"
 
     let res = await fetch("https://api.adviceslip.com/advice")
     let data = await res.json()
-
+    loader.classList.remove("my");
     h1.innerHTML = data.slip.advice
-    
+    noli.innerHTML = data.slip.id
 
 
-    console.log(data.slip.advice)
+    console.log(data.slip.id)
 }
